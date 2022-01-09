78875b8492294228f359a8a7f616355f907014c9
jaschn <49870201+jaschn@users.noreply.github.com>
2022-01-09 14:24:40 +0100

    Parametize test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,18 +1,7 @@
-from src.data.dataset import CorruptMNISTset
-from hydra import initialize, compose
-import pytest
 import torchvision
+import pytest
 
-def test_dataset_train():
-    with initialize(config_path="../config"):
-        cfg = compose(config_name="training_conf.yaml")
-        
-    dataset = torchvision.datasets.MNIST("data", download=True, train=True)
-    assert len(dataset) == 60000, "dataset length wrong"
-
-def test_dataset_test():
-    with initialize(config_path="../config"):
-        cfg = compose(config_name="training_conf.yaml")
-
-    dataset_test = torchvision.datasets.MNIST("data", download=True, train=False)
-    assert len(dataset_test) == 10000, "testset length wrong"
+@pytest.mark.parametrize("type, cnt", [(True, 60000), (False, 10000)])
+def test_dataset_train(type, cnt):
+    dataset = torchvision.datasets.MNIST("data", download=True, train=type)
+    assert len(dataset) == cnt, "dataset length wrong"
