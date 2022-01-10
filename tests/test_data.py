141afedf751d5a11c1c73a1f9a04e171412eee7a
jaschn <49870201+jaschn@users.noreply.github.com>
2022-01-10 10:34:25 +0100

    Updated data test to skip if data not present

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,12 @@
 import pytest
-import torchvision
+import os
+from src.data.dataset import mnist
+import hydra
 
-
-@pytest.mark.parametrize("type, cnt", [(True, 60000), (False, 10000)])
+@pytest.mark.skipif(not os.path.exists("data"), reason="Data files not found")
+@pytest.mark.parametrize("type, cnt", [("train", 40000), ("test", 5000)])
 def test_dataset_train(type, cnt):
-    dataset = torchvision.datasets.MNIST("data", download=True, train=type)
-    assert len(dataset) == cnt, "dataset length wrong"
+    with hydra.initialize(config_path="../config"):
+        cfg = hydra.compose(config_name="training_conf.yaml")
+    dataloader = mnist(cfg.training.input_filepath, type=type)
+    assert len(dataloader.dataset) == cnt, "dataset length wrong"
